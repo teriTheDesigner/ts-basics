f492435b1459616e34c7f8621e071a499eb1461a
teriTheDesigner <tere1109@stud.kea.dk>
2024-10-08 20:03:32 +0200

    creating interfaces

--- src/demo.ts
+++ src/demo.ts
@@ -1,8 +1,23 @@
-class Contact {
-  id;
-  name;
-  birthDate;
+interface Contact extends Address {
+  id: number;
+  name: string;
+  birthDate?: Date;
 }
+
+interface Address {
+  city: string;
+  country: string;
+  zipCode: string;
+}
+
+let primaryContact: Contact = {
+  // birthDate: new Date("02-05-2024"),
+  id: 23876,
+  name: "Jamie",
+  city: "Madrid",
+  country: "Spain",
+  zipCode: "28002",
+};
 
 let x: number;
 let y: string;
